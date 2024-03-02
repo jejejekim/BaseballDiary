e0811fc84e6b3e8f4485cea576ed2b3f2bae4102
jejejekim <46785917+jejejekim@users.noreply.github.com>
2024-03-03 01:14:43 +0900

    Feat: Crawling score, batter, pitcher

--- my-baseball-diary/KBOCrawling.py
+++ my-baseball-diary/KBOCrawling.py
@@ -49,6 +49,21 @@ for value in driver.find_elements(By.XPATH, '//*[@id="contents"]/div[3]/div/div[
     start_time = value.text
     print(start_time)
 
+#이닝점수
+away_score =[]
+home_score=[]
+
+score_rows = driver.find_elements(By.XPATH, '//*[@id="tblScordboard2"]/tbody/tr')
+away_score_body= score_rows[0].find_elements(By.TAG_NAME, "td")
+for index, value in enumerate(away_score_body):
+    away_score.append(value.text)
+
+home_score_body= score_rows[1].find_elements(By.TAG_NAME, "td")
+for index, value in enumerate(home_score_body):
+    home_score.append(value.text)
+
+print("awayScore: ", away_score, "homeScore: ", home_score)
+
 #원정팀
 for value in driver.find_elements(By.XPATH, '//*[@id="contents"]/div[3]/div/div[1]/ul/li[2]/div[2]/div[2]/div[1]/div[1]/img'):
     away_team = value.get_attribute('alt')
@@ -58,3 +73,27 @@ for value in driver.find_elements(By.XPATH, '//*[@id="contents"]/div[3]/div/div[
 for value in driver.find_elements(By.XPATH, '//*[@id="contents"]/div[3]/div/div[1]/ul/li[2]/div[2]/div[2]/div[3]/div[1]/img'):
     home_team = value.get_attribute('alt')
     print(home_team)
+
+#야수라인업
+batter =[]
+batter_table = driver.find_element(By.ID, 'tblHomeHitter1')
+batter_tbody = batter_table.find_element(By.TAG_NAME, "tbody")
+batter_rows = batter_tbody.find_elements(By.TAG_NAME, "tr")
+for index, value in enumerate(batter_rows):
+    batter_num = value.find_elements(By.TAG_NAME, "th")[0].text
+    position = value.find_elements(By.TAG_NAME, "th")[1].text
+    batter_name = value.find_elements(By.TAG_NAME, "td")[0].text
+    batter.append({"num": batter_num, "position": position, "name": batter_name})
+
+#투수라인업
+pitcher = []
+pitcher_table = driver.find_element(By.XPATH, '//*[@id="tblHomePitcher"]')
+pitcher_tbody = pitcher_table.find_element(By.TAG_NAME, "tbody")
+pitcher_rows = pitcher_tbody.find_elements(By.TAG_NAME, "tr")
+for index, value in enumerate(pitcher_rows):
+    pitcher_record = value.find_elements(By.TAG_NAME, "td")[2].text
+    pitcher_name = value.find_elements(By.TAG_NAME, "td")[0].text
+    pitcher.append({"record": pitcher_record, "name": pitcher_name})
+
+print(batter)
+print(pitcher)
