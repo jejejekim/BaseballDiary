7369bc4f8c2958eb9b4d0c53267569532890aff2
jejejekim <46785917+jejejekim@users.noreply.github.com>
2024-03-03 23:25:22 +0900

    Fix: Crawling data form(to list)

--- my-baseball-diary/KBOCrawling.py
+++ my-baseball-diary/KBOCrawling.py
@@ -21,7 +21,7 @@ driver = webdriver.Chrome(options=chrome_options)
 driver.get('https://www.koreabaseball.com/Schedule/GameCenter/Main.aspx')
 
 # 페이지가 완전히 로딩되도록 3초동안 기다림
-time.sleep(3)
+time.sleep(4)
 
 #캘린더 버튼 클릭
 driver.find_element(By.XPATH, '//*[@id="contents"]/div[2]/div/img').click()
@@ -31,13 +31,13 @@ driver.find_element(By.XPATH, '//*[@id="ui-datepicker-div"]/div/div/select[2]/op
 driver.find_element(By.XPATH, '//*[@id="ui-datepicker-div"]/div/div/select[1]/option[5]').click()
 #날짜 선택
 driver.find_element(By.XPATH, '//*[@id="ui-datepicker-div"]/table/tbody/tr[4]/td[6]/a').click()
-time.sleep(2)
+time.sleep(3)
 
 #팀 선택
 driver.find_element(By.XPATH, '//*[@id="contents"]/div[3]/div/div[1]/ul/li[2]').click()
 #리뷰 탭 선택
 driver.find_element(By.XPATH, '//*[@id="tabDepth2"]/li[2]/a').click()
-time.sleep(2)
+time.sleep(3)
 
 #구장
 for value in driver.find_elements(By.XPATH, '//*[@id="contents"]/div[3]/div/div[1]/ul/li[2]/div[1]/ul/li[1]'):
@@ -47,35 +47,30 @@ for value in driver.find_elements(By.XPATH, '//*[@id="contents"]/div[3]/div/div[
 #시작 시간
 for value in driver.find_elements(By.XPATH, '//*[@id="contents"]/div[3]/div/div[1]/ul/li[2]/div[1]/ul/li[2]'):
     start_time = value.text
-    print(start_time)
 
-#이닝점수
-away_score =[]
-home_score=[]
-
-score_rows = driver.find_elements(By.XPATH, '//*[@id="tblScordboard2"]/tbody/tr')
-away_score_body= score_rows[0].find_elements(By.TAG_NAME, "td")
+#이닝 점수
+away_score = []
+away_score_rows = driver.find_element(By.XPATH, '//*[@id="tblScordboard2"]/tbody/tr[1]')
+away_score_body= away_score_rows.find_elements(By.TAG_NAME, "td")
 for index, value in enumerate(away_score_body):
-    away_score.append(value.text)
+    away_score.append(value.text)    
 
-home_score_body= score_rows[1].find_elements(By.TAG_NAME, "td")
+home_score = []
+home_score_rows = driver.find_element(By.XPATH, '//*[@id="tblScordboard2"]/tbody/tr[2]')
+home_score_body= home_score_rows.find_elements(By.TAG_NAME, "td")
 for index, value in enumerate(home_score_body):
     home_score.append(value.text)
-
-print("awayScore: ", away_score, "homeScore: ", home_score)
 
 #원정팀
 for value in driver.find_elements(By.XPATH, '//*[@id="contents"]/div[3]/div/div[1]/ul/li[2]/div[2]/div[2]/div[1]/div[1]/img'):
     away_team = value.get_attribute('alt')
-    print(away_team)
 
 #홈팀
 for value in driver.find_elements(By.XPATH, '//*[@id="contents"]/div[3]/div/div[1]/ul/li[2]/div[2]/div[2]/div[3]/div[1]/img'):
     home_team = value.get_attribute('alt')
-    print(home_team)
 
 #야수라인업
-batter =[]
+batter = []
 batter_table = driver.find_element(By.ID, 'tblHomeHitter1')
 batter_tbody = batter_table.find_element(By.TAG_NAME, "tbody")
 batter_rows = batter_tbody.find_elements(By.TAG_NAME, "tr")
@@ -95,5 +90,16 @@ for index, value in enumerate(pitcher_rows):
     pitcher_name = value.find_elements(By.TAG_NAME, "td")[0].text
     pitcher.append({"record": pitcher_record, "name": pitcher_name})
 
-print(batter)
-print(pitcher)
+KBOdatas = list()
+
+#모든 정보를 저장할 KBOData 배열
+KBOData ={
+          "startTime": start_time,
+          "score": {"away": away_score, "home": home_score },
+          "team": {"away": away_team, "home": home_team},
+          "batter": batter,
+          "pitcher": pitcher,
+        }
+
+KBOdatas.append(KBOData)
+print(KBOdatas)
